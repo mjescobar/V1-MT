c67ada62151248ea15ce9144423608f0fada3b70
ptoledo <ptoledo@186dhcp191.pl.eso.org>
2015-10-25 16:59:57 -0300

    NeuronmentTesting v0.1 - Changing project name from NeuronmentRegressions

---
+++ NeuronmentTesting/src/neuronmentregression.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "ptoledo"
+__date__ = "$Oct 25, 2015 2:58:14 PM$"
+
+if __name__ == "__main__":
+  print "Hello World"
