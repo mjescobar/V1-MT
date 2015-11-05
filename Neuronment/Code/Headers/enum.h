13bad4207c3dc4dff10a43548d2792cd32590745
ptoledo <ptoledo@fe80:0:0:0:28be:33ff:febe:af9b%9>
2015-11-05 09:53:53 -0300

    Updating enum to new code standards Deleting ReturnSuccessWarning because unused in new implementation (this will broke the build)

--- Neuronment/Code/Headers/enum.h
+++ Neuronment/Code/Headers/enum.h
@@ -6,17 +6,16 @@
 enum ReturnType {
   ReturnNone,
   ReturnSuccess,
-  ReturnSuccessWarning,
   ReturnFail
 };
 
 /** Used to indicate the message type for output */
 enum MessageType {
-  MessageInformation = 0,
+  MessageInformation      = 0,
   MessageCodedInformation = 5,
-  MessageCoded = 10,
-  MessageDevelopment = 20,
-  MessageAllways = 30
+  MessageCoded            = 10,
+  MessageDevelopment      = 20,
+  MessageAllways          = 30
 };
 
 enum RedirectionType {
@@ -24,52 +23,5 @@ enum RedirectionType {
   RedirectionNew,
   RedirectionAppend
 };
-
-#if 0
-
-/** Types of neuron */
-enum NeuronType {
-  Neuron_ND,
-  Neuron_V1,
-  Neuron_MT
-};
-
-/** Used to indicate direction for string edition */
-enum SideType {
-  Side_Right,
-  Side_Left
-};
-
-/** Used to indicate sense of data at reporting */
-enum OrientationType {
-  Orientation_Horizontal,
-  Orientation_Vertical
-};
-
-/** To process three state output functions */
-enum ResultType {
-  Result_true,
-  Result_false,
-  Result_silent
-};
-
-/** To process command lines */
-enum ArgumentType {
-  Directive,
-  SubDirective,
-  Flag,
-  FlaggedAttribute
-};
-
-/** Data types for use and processing */
-enum DataType {
-  Data_null,
-  Data_int,
-  Data_bool,
-  Data_voidp,
-  Data_double,
-  Data_string
-};
-#endif
 
 #endif
