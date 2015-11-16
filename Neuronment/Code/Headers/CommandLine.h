c1220157471f832ec1917cbf70cbdc9ff81e2114
Pedro F. Toledo <pedrotoledocorrea@gmail.com>
2015-11-16 15:47:09 -0300

    Updating CommandLine to new code standards

--- Neuronment/Code/Headers/CommandLine.h
+++ Neuronment/Code/Headers/CommandLine.h
@@ -10,46 +10,170 @@ using namespace std;
 
 class CommandLine {
 public:
-  //Standard methods
+  /**
+   * Empty constructor
+   * Initializes the hash table with COMMAND_MANAGER_HASH_SIZE slots
+   */
   CommandLine();
+
+  /**
+   * Copy constructor
+   * @param orig
+   */
   CommandLine(const CommandLine& orig);
+
+  /**
+   * Destructor
+   */
   virtual ~CommandLine();
-  CommandLine & operator=(const CommandLine & orig); //Lothar: to implement
-  //Special constructor
+
+  /**
+   * Assignment operator
+   * @param orig
+   * @return 
+   */
+  CommandLine & operator=(const CommandLine & orig);
+
+  /**
+   * Command line constructor
+   * Constructs a CommandLine instance starting from the main arguments
+   * @param argcP The argc main argument
+   * @param argvP The argv main argument
+   */
   CommandLine(int argcP, char** argvP);
-  //Command line process methods
+
+  /**
+   * Prepares the instance to process a new set of strings
+   * @return 
+   */
   ReturnType Restart();
-  ReturnType Append(int argc, char** argv);
+
+  /**
+   * Appends the string to the command to process
+   * @param  StringP String to add
+   * @return 
+   */
   ReturnType Append(string StringP);
-  //Command line data methods
+
+  /**
+   * Returns ReturnSuccess if a flag indicated by FlagP exists
+   * @param FlagP The flag to check
+   * @return 
+   */
   ReturnType GetFlag(string FlagP);
+
+  /**
+   * Returns the value stored on the indicated flag
+   * @param FlagP  The flag to retrieve the value
+   * @param ValueP The pointer where the value will be stored
+   * @return 
+   */
   ReturnType GetFlag(string FlagP, string &ValueP);
+
+  /**
+   * Returns the full command formed after appends including redirections
+   * @param CommandP The pointer to store the command
+   * @return 
+   */
   ReturnType GetCommand(string &CommandP);
+
+  /**
+   * Returns a string formed by the directive or the directive and subdirective
+   * separated by a Separator string
+   * @param CleanCommandP The pointer to return the value
+   * @param Separator     The string to separate the directive and subdirective
+   * @return 
+   */
   ReturnType GetCleanCommand(string &CleanCommandP, string Separator = " ");
+
+  /**
+   * Returns the redirection file indicated on the command
+   * @param RedirectionP The pointer to return the file name
+   * @return 
+   */
   ReturnType GetRedirection(string &RedirectionP);
+
+  /**
+   * Retrieves the redirection type it if exists
+   * @param RedirectionModeP
+   * @return 
+   */
   ReturnType GetRedirectionType(RedirectionType &RedirectionModeP);
-  //Command line information methods
+
+  /**
+   * Indicates if the command is ready for analysis
+   * @param ReadyP The pointer to return the status
+   * @return
+   */
   ReturnType IsReady(bool &ReadyP);
+
+  /**
+   * Indicates if the command line has a command
+   * @param HasCommandP The pointer to return the value
+   * @return
+   */
   ReturnType HasCommand(bool &HasCommandP);
+
+  /**
+   * Indicates if the command line has a comment
+   * @param HasCommentP The pointer to return the value
+   * @return
+   */
   ReturnType HasComment(bool &HasCommentP);
+
+  /**
+   * Indicates if the command has a redirection
+   * @param HasRedirectionP The pointer to return the value
+   * @return
+   */
   ReturnType HasRedirection(bool &HasRedirectionP);
+
 private:
-  //Methods
+
+  /**
+   * Appends the first argc elements from argv to the command to process
+   * @param argcP The amount of elements to add
+   * @param argvP The array containing the elements to add
+   * @return
+   */
+  ReturnType Append(int argcP, char** argvP);
+
+  /**
+   * Returns the comment stored on the instance
+   * @param CommentP The pointer to return the value
+   * @return
+   */
   ReturnType GetComment(string &CommentP);
+
+  /**
+   * Return the full line stored on the instance
+   * @param FullLine The pointer to return the value
+   * @return
+   */
   ReturnType GetFullLine(string &FullLine);
-  ReturnType GetDirectives(vector<string> &DirectivesP);
-  ReturnType ProcessFullCommand();
+
+  /**
+   * Returns a vector containing all the allowed characters on a string
+   * @param AllowedChars Pointer to return the list
+   * @return
+   */
   ReturnType GetAllowedChars(vector<string> &AllowedChars);
-  //Members
-  bool Ready;
-  string FullLine;
-  string FullCommand;
-  string Command;
-  string Redirection;
-  string Comment;
-  RedirectionType RedirectionMode;
-  vector<string> Directives;
+
+  /**
+   * Processes the command string and sets the ready status
+   * @return
+   */
+  ReturnType ProcessFullCommand();
+
+  bool               Ready;
+  string             FullLine;
+  string             FullCommand;
+  string             Command;
+  string             Redirection;
+  string             Comment;
+  RedirectionType    RedirectionMode;
+  vector<string>     Directives;
   HashTable<string> *FlagValues;
-};
+} ;
 
 #endif
--- Neuronment/Code/Sources/CommandLine.cpp
+++ Neuronment/Code/Sources/CommandLine.cpp
@@ -19,7 +19,16 @@ CommandLine::CommandLine()
 
 CommandLine::CommandLine(const CommandLine& orig)
 {
-  Log.CodedMessage("DV-008: CommandManager");
+  Log.CodedMessage("DV-001: CommandManager copy constructor");
+}
+
+CommandLine::~CommandLine()
+{
+  delete FlagValues;
+}
+
+CommandLine & CommandLine::operator=(const CommandLine & orig){
+  Log.CodedMessage("DV-001: CommandManager assignment operator");
 }
 
 CommandLine::CommandLine(int argcP, char** argvP)
@@ -35,26 +44,18 @@ CommandLine::CommandLine(int argcP, char** argvP)
   Append(argcP, argvP);
 }
 
-CommandLine::~CommandLine()
+ReturnType CommandLine::Restart()
 {
+  Ready = false;
+  FullLine = "";
+  FullCommand = "";
+  Command = "";
+  Redirection = "";
+  Comment = "";
+  Directives.clear();
+  RedirectionMode = RedirectionNone;
   delete FlagValues;
-}
-
-ReturnType CommandLine::Append(int argc, char** argv)
-{
-  ReturnCatch(DataCheck(argv, "NotNull"));
-  string Tmp = "";
-  for (int i = 0; i < argc; i++) {
-    Tmp = Tmp + argv[i];
-    if (i + 1 < argc) {
-      Tmp = Tmp + " ";
-    }
-  }
-  if (DataCheck(Tmp, "NonEmpty") == ReturnFail) {
-    Log.CodedMessage("SD-011");
-    return ReturnFail;
-  }
-  ReturnCatch(Append(Tmp));
+  FlagValues = new HashTable<string>(COMMAND_MANAGER_HASH_SIZE);
   return ReturnSuccess;
 }
 
@@ -69,27 +70,210 @@ ReturnType CommandLine::Append(string StringP)
   if (DataCheck(FullCommand[FullCommand.size() - 1], "IsBackSlash") == ReturnSuccess) {
     Ready = false;
     FullCommand.substr(0, FullCommand.size() - 1);
+  } else {
+    ProcessFullCommand();
+  }
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetFlag(string FlagP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  string Dummy; //Lothar: straight getentryquick types?
+  if (FlagValues->GetEntryQuick(FlagP, Dummy) == ReturnSuccess) {
     return ReturnSuccess;
   } else {
-    ReturnCatch(ProcessFullCommand());
-    return ReturnSuccess;
+    return ReturnFail;
   }
+}
+
+ReturnType CommandLine::GetFlag(string FlagP, string &ValueP) //Lothar: how list flags are processed?
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  if (FlagValues->GetEntryQuick(FlagP, ValueP) == ReturnSuccess) {
+    return ReturnSuccess;
+  } else {
+    return ReturnFail;
+  }
+}
+
+ReturnType CommandLine::GetCommand(string &CommandP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  CommandP = Command;
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetCleanCommand(string &CleanCommandP, string Separator)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  CleanCommandP = "";
+  for (int i = 0; i < Directives.size(); i++) {
+    if (CleanCommandP.empty()) {
+      CleanCommandP = Directives[0];
+    } else {
+      CleanCommandP = CleanCommandP + Separator + Directives[i];
+    }
+  }
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetRedirection(string &RedirectionP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  RedirectionP = Redirection;
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetRedirectionType(RedirectionType &RedirectionModeP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  RedirectionModeP = RedirectionMode;
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::IsReady(bool &ReadyP)
+{
+  ReadyP = Ready;
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::HasCommand(bool &HasCommandP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  if (Command.length() > 0) {
+    HasCommandP = true;
+  } else {
+    HasCommandP = false;
+  }
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::HasComment(bool &HasCommentP)
+{
+  if (Ready) {
+    if (Comment.length() > 0) {
+      HasCommentP = true;
+    } else {
+      HasCommentP = false;
+    }
+    return ReturnSuccess;
+  } else {
+    return Return(ReturnFail, "DV-024");
+  }
+}
+
+ReturnType CommandLine::HasRedirection(bool &HasRedirectionP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  if (Redirection.length() > 0) {
+    HasRedirectionP = true;
+  } else {
+    HasRedirectionP = false;
+  }
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::Append(int argcP, char** argvP)
+{
+  DataCheck(argvP, "NotNull");
+  string Tmp = "";
+  for (int i = 0; i < argcP; i++) {
+    Tmp = Tmp + argvP[i];
+    if (i + 1 < argcP) {
+      Tmp = Tmp + " ";
+    }
+  }
+  if (DataCheck(Tmp, "NonEmpty") == ReturnFail) {
+    return Return(ReturnFail, "SD-011");
+  }
+  Append(Tmp);
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetComment(string &CommentP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  CommentP = Comment;
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetFullLine(string &FullLineP)
+{
+  if (!Ready) {
+    return Return(ReturnFail, "DV-024");
+  }
+  FullLineP = FullLine;
+  return ReturnSuccess;
+}
+
+ReturnType CommandLine::GetAllowedChars(vector<string>& AllowedCharsP)
+{
+  AllowedCharsP.clear();
+  AllowedCharsP.push_back("IsTab");
+  AllowedCharsP.push_back("IsSpace");
+  AllowedCharsP.push_back("IsNumberSign");
+  AllowedCharsP.push_back("IsHyphen");
+  AllowedCharsP.push_back("IsDot");
+  AllowedCharsP.push_back("IsSlash");
+  AllowedCharsP.push_back("IsNumber");
+  AllowedCharsP.push_back("IsLessThan");
+  AllowedCharsP.push_back("IsGreaterThan");
+  AllowedCharsP.push_back("IsLetter");
+  AllowedCharsP.push_back("IsBackSlash");
+  AllowedCharsP.push_back("IsUnderscore");
+  AllowedCharsP.push_back("IsCurlyBraces");
+  AllowedCharsP.push_back("IsColon");
+  return ReturnSuccess;
 }
 
 ReturnType CommandLine::ProcessFullCommand()
 {
-  // Storing the full line
+  FullLine = FullCommand;
+  // Processing the line continuation character
   for (int i = 0; i < FullCommand.size(); i++) {
     if (DataCheck(FullCommand[i], "IsBackSlash") == ReturnSuccess) {
       FullCommand[i] = ' ';
     }
   }
-  FullLine = FullCommand;
-  ReturnCatch(trim(FullCommand));
+  trim(FullCommand);
   if (FullCommand.size()) {
-    // Removing comments from line
+    // Check that all the characters are authorized
+    vector<string> AllowedChars;
+    GetAllowedChars(AllowedChars);
+    for (int i = 0; i < Command.size(); i++) {
+      bool Allowed = false;
+      for (int j = 0; j < AllowedChars.size(); j++) {
+        if (DataCheck(Command[i], AllowedChars[j]) == ReturnSuccess) {
+          Allowed = true;
+          j = AllowedChars.size();
+        }
+      }
+      if (!Allowed) {
+        return Return(ReturnFail, "IN-011");
+      }
+    }
+    // Processing comments
     string LookingForComment = FullCommand;
-    ReturnCatch(trim(LookingForComment));
+    trim(LookingForComment);
     for (int i = 0; i < LookingForComment.size(); i++) {
       if (DataCheck(LookingForComment[i], "IsNumberSign") == ReturnSuccess) {
         if (i > 0) {
@@ -102,7 +286,7 @@ ReturnType CommandLine::ProcessFullCommand()
         }
       }
     }
-    // Dividing string for redirection
+    // Processing redirection
     Command = "";
     Redirection = "";
     bool RedirectionDetected = false;
@@ -134,40 +318,24 @@ ReturnType CommandLine::ProcessFullCommand()
       } else {
         RedirectionMode = RedirectionNew;
       }
-      ReturnCatch(trim(Redirection));
+      trim(Redirection);
       vector<string> InternalRedirectionTokenize;
       ReturnCatch(Tokenize(Redirection, InternalRedirectionTokenize));
       if (InternalRedirectionTokenize.size() > 1) {
         Log.CodedMessage("IN-003");
         Redirection = InternalRedirectionTokenize[0];
       }
-      ReturnCatch(trim(Redirection));
-      ReturnCatch(Tokenize(Redirection, InternalRedirectionTokenize));
+      trim(Redirection);
+      Tokenize(Redirection, InternalRedirectionTokenize);
       if (InternalRedirectionTokenize.size() > 1) {
         Log.CodedMessage("IN-003");
         Redirection = InternalRedirectionTokenize[0];
       }
     }
-    ReturnCatch(trim(Redirection));
-    // Check that all the characters are authorized
-    vector<string> AllowedChars;
-    ReturnCatch(GetAllowedChars(AllowedChars));
-    for (int i = 0; i < Command.size(); i++) {
-      bool Allowed = false;
-      for (int j = 0; j < AllowedChars.size(); j++) {
-        if (DataCheck(Command[i], AllowedChars[j]) == ReturnSuccess) {
-          Allowed = true;
-          j = AllowedChars.size();
-        }
-      }
-      if (!Allowed) {
-        Log.CodedMessage("IN-011");
-        return ReturnFail;
-      }
-    }
+    trim(Redirection);
     // Tokenizing and storing words
     vector<string> Tokens;
-    ReturnCatch(Tokenize(Command, Tokens));
+    Tokenize(Command, Tokens);
     bool InPreamble = true;
     bool InFlag = false;
     bool InList = false;
@@ -186,8 +354,7 @@ ReturnType CommandLine::ProcessFullCommand()
           Value = "";
           Flag = Token.substr(1, Token.size() - 1);
           if (Flag.size() < 1) {
-            Log.CodedMessage("IN-014");
-            return ReturnFail;
+            return Return(ReturnFail, "IN-014");
           }
         }
       } else {
@@ -223,8 +390,7 @@ ReturnType CommandLine::ProcessFullCommand()
               if (Token.size() == 1) {
                 InList = true;
               } else {
-                Log.CodedMessage("IN-013");
-                return ReturnFail;
+                return Return(ReturnFail, "IN-013");
               }
             } else {
               InFlag = false;
@@ -234,8 +400,7 @@ ReturnType CommandLine::ProcessFullCommand()
                 Value = "";
                 Flag = Token.substr(1, Token.size() - 1);
                 if (Flag.size() < 1) {
-                  Log.CodedMessage("IN-014");
-                  return ReturnFail;
+                  return Return(ReturnFail, "IN-014");
                 }
               } else {
                 FlagValues->PutEntryQuick(Flag, Token);
@@ -247,20 +412,18 @@ ReturnType CommandLine::ProcessFullCommand()
             InFlag = true;
             Flag = Token.substr(1, Token.size() - 1);
             if (Flag.size() < 1) {
-              Log.CodedMessage("IN-014");
-              return ReturnFail;
+              return Return(ReturnFail, "IN-014");
             }
             Value = "";
           } else {
-            Log.CodedMessage("IN-012");
-            return ReturnFail;
+            return Return(ReturnFail, "IN-012");
           }
         }
       }
     }
     if (InList) {
-      Log.CodedMessage("IN-015");
-      return ReturnFail;
+      return Return(ReturnFail, "IN-015");
+
     }
     if (InFlag) {
       FlagValues->PutEntryQuick(Flag, "");
@@ -277,195 +440,4 @@ ReturnType CommandLine::ProcessFullCommand()
     Comment = "";
     return ReturnSuccess;
   }
-}
-
-ReturnType CommandLine::GetAllowedChars(vector<string>& AllowedCharsP)
-{
-  AllowedCharsP.clear();
-  AllowedCharsP.push_back("IsTab");
-  AllowedCharsP.push_back("IsSpace");
-  AllowedCharsP.push_back("IsNumberSign");
-  AllowedCharsP.push_back("IsHyphen");
-  AllowedCharsP.push_back("IsDot");
-  AllowedCharsP.push_back("IsSlash");
-  AllowedCharsP.push_back("IsNumber");
-  AllowedCharsP.push_back("IsLessThan");
-  AllowedCharsP.push_back("IsGreaterThan");
-  AllowedCharsP.push_back("IsLetter");
-  AllowedCharsP.push_back("IsBackSlash");
-  AllowedCharsP.push_back("IsUnderscore");
-  AllowedCharsP.push_back("IsCurlyBraces");
-  AllowedCharsP.push_back("IsColon");
-  return ReturnSuccess;
-}
-
-ReturnType CommandLine::IsReady(bool &ReadyP)
-{
-  ReadyP = Ready;
-  return ReturnSuccess;
-}
-
-ReturnType CommandLine::GetFlag(string FlagP, string &ValueP)
-{
-  if (Ready) {
-    return FlagValues->GetEntryQuick(FlagP, ValueP);
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetFlag(string FlagP)
-{
-  if (Ready) {
-    string Dummy;
-    return FlagValues->GetEntryQuick(FlagP, Dummy);
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetDirectives(vector<string> &DirectivesP)
-{
-  if (Ready) {
-    DirectivesP = Directives;
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetCleanCommand(string &CleanCommandP, string Separator)
-{
-  if (Ready) {
-    CleanCommandP = "";
-    for (int i = 0; i < Directives.size(); i++) {
-      if (CleanCommandP.empty()) {
-        CleanCommandP = Directives[0];
-      } else {
-        CleanCommandP = CleanCommandP + Separator + Directives[i];
-      }
-    }
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetComment(string &CommentP)
-{
-  if (Ready) {
-    CommentP = Comment;
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetCommand(string &CommandP)
-{
-  if (Ready) {
-    CommandP = Command;
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetRedirection(string &RedirectionP)
-{
-  if (Ready) {
-    RedirectionP = Redirection;
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetRedirectionType(RedirectionType &RedirectionModeP)
-{
-  if (Ready) {
-    RedirectionModeP = RedirectionMode;
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::GetFullLine(string &FullLineP)
-{
-  if (Ready) {
-    FullLineP = FullLine;
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::HasCommand(bool &HasCommandP)
-{
-  if (Ready) {
-    if (Command.length() > 0) {
-      HasCommandP = true;
-    } else {
-      HasCommandP = false;
-    }
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::HasComment(bool &HasCommentP)
-{
-  if (Ready) {
-    if (Comment.length() > 0) {
-      HasCommentP = true;
-    } else {
-      HasCommentP = false;
-    }
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::HasRedirection(bool &HasRedirectionP)
-{
-  if (Ready) {
-    if (Redirection.length() > 0) {
-      HasRedirectionP = true;
-    } else {
-      HasRedirectionP = false;
-    }
-    return ReturnSuccess;
-  } else {
-    Log.CodedMessage("DV-024");
-    return ReturnFail;
-  }
-}
-
-ReturnType CommandLine::Restart()
-{
-  Ready = false;
-  FullLine = "";
-  FullCommand = "";
-  Command = "";
-  Redirection = "";
-  Comment = "";
-  Directives.clear();
-  RedirectionMode = RedirectionNone;
-  delete FlagValues;
-  FlagValues = new HashTable<string>(COMMAND_MANAGER_HASH_SIZE);
-  return ReturnSuccess;
 }
